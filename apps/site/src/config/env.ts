7f5f00aabba3db20656c35aa8b1b6898d967ee88
minhtungo <loducfc@gmail.com>
2024-10-20 20:04:06 -0400

    fe:feat: add shadcn, safe environment and next-safe-action to the frontend

---
+++ apps/site/src/config/env.ts
@@ -0,0 +1,19 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    PORT: z.string().optional(),
+    NODE_ENV: z.string().optional(),
+    SERVER_URL: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_BASE_URL: z.string(),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_API_URL,
+    SERVER_URL: process.env.SERVER_URL,
+  },
+});
